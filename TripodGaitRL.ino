124e13adc02025aeb07a48c58595c8a974e34122
Muhammad Rafli <154863009+raaflii@users.noreply.github.com>
2025-04-05 01:41:43 +0700

    testing tripod gait with right leg

---
+++ TripodGaitRL.ino
@@ -0,0 +1,154 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// true = do stance phase, false = do swing phase
+struct Leg {
+  int ch[3];
+  bool state;
+};
+
+Leg R1 = {{0, 1, 2}, true};
+Leg R2 = {{4, 5, 6}, false};
+Leg R3 = {{8, 9, 10}, true};
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+const int SERVOMIN = 102; 
+const int SERVOMAX = 512;
+
+const float coxa_len  = 43.0;
+const float femur_len = 60.0;
+const float tibia_len = 104.0;
+
+float P0[3] = {80, -50, -50};  
+float P1[3] = {80, -30, -30};  
+float P2[3] = {80, 30, -30};  
+float P3[3] = {80, 50, -50};
+
+int steps = 20;
+
+float constrainAngle(float val) {
+  return constrain(val, 0, 180);
+}
+
+void bezier(float t, float P0[3], float P1[3], float P2[3], float P3[3], float &x, float &y, float &z) {
+  float u = 1 - t;
+  x = u*u*u * P0[0] + 3 * u*u * t * P1[0] + 3 * u * t*t * P2[0] + t*t*t * P3[0];
+  y = u*u*u * P0[1] + 3 * u*u * t * P1[1] + 3 * u * t*t * P2[1] + t*t*t * P3[1];
+  z = u*u*u * P0[2] + 3 * u*u * t * P1[2] + 3 * u * t*t * P2[2] + t*t*t * P3[2];
+}
+
+void inverseKinematic(float x, float y, float z, float &sudutCoxa, float &sudutFemur, float &sudutTibia) {
+  sudutCoxa = degrees(atan2(y, x));
+  float jarakHorizontal = sqrt(x * x + y * y) - coxa_len;
+  float femur_1 = degrees(atan2(z, jarakHorizontal));
+  float a = sqrt(z * z + jarakHorizontal * jarakHorizontal);
+  a = min(a, femur_len + tibia_len);
+  a = max(a, fabs(femur_len - tibia_len));
+  
+  float ratio = (femur_len * femur_len + a * a - tibia_len * tibia_len) / (2.0 * a * femur_len);
+  ratio = constrain(ratio, -1, 1);
+  float femur_2 = degrees(acos(ratio));
+  
+  sudutFemur = femur_1 + femur_2;
+  
+  float ratio2 = (femur_len * femur_len + tibia_len * tibia_len - a * a) / (2.0 * femur_len * tibia_len);
+  ratio2 = constrain(ratio2, -1, 1);
+  sudutTibia = degrees(acos(ratio2)) - 90.0;
+}
+
+void kakiKanan(float x, float y, float z, float &servoCoxa, float &servoFemur, float &servoTibia) {
+  float c, f, t;
+  inverseKinematic(x, y, z, c, f, t);
+  
+  servoCoxa  = constrainAngle(90.0 - c);
+  servoFemur = constrainAngle(90.0 + f);
+  servoTibia = constrainAngle(90.0 - t);
+}
+
+void kakiKiri(float x, float y, float z, float &servoCoxa, float &servoFemur, float &servoTibia) {
+  float c, f, t;
+  inverseKinematic(x, y, z, c, f, t);
+  
+  servoCoxa  = constrainAngle(270.0 - c);
+  servoFemur = constrainAngle(90.0 - f);
+  servoTibia = constrainAngle(90.0 + t);
+}
+
+int angleToPulse(float angle) {
+  return SERVOMIN + (angle / 180.0) * (SERVOMAX - SERVOMIN);
+}
+
+void stancePhase(float &y, float &servoCoxa, float &servoFemur, float &servoTibia) {
+  float stanceY = P0[1] - P3[1];
+  float stance[3] = {80, stanceY, -50};
+  float stepSize = stanceY / steps;
+
+  kakiKanan(stance[0], y, stance[2], servoCoxa, servoFemur, servoTibia);
+  y += stepSize;
+}
+
+void swingPhase(float t, float &servoCoxa, float &servoFemur, float &servoTibia) {
+  float x, y, z;
+  bezier(t, P0, P1, P2, P3, x, y, z);
+
+  kakiKanan(x, y, z, servoCoxa, servoFemur, servoTibia);
+}
+
+void doMovement(Leg &leg, float t, float y) { 
+  float servoCoxa, servoFemur, servoTibia;
+
+  if (leg.state == 0) {
+    stancePhase(y, servoCoxa, servoFemur, servoTibia);
+  } else {
+    swingPhase(t, servoCoxa, servoFemur, servoTibia);
+  }
+
+  int pulseCoxa  = angleToPulse(servoCoxa);
+  int pulseFemur = angleToPulse(servoFemur);
+  int pulseTibia = angleToPulse(servoTibia);
+
+  pwm.setPWM(leg.ch[0], 0, pulseCoxa);
+  pwm.setPWM(leg.ch[1], 0, pulseFemur);
+  pwm.setPWM(leg.ch[2], 0, pulseTibia);
+
+  leg.state = !leg.state;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Tripod Gait Right Leg");
+  pwm.begin();
+  pwm.setPWMFreq(50);
+  delay(10);
+
+  int pulseCoxa  = angleToPulse(80);
+  int pulseFemur = angleToPulse(0);
+  int pulseTibia = angleToPulse(-50);
+
+  pwm.setPWM(R1.ch[0], 0, pulseCoxa);
+  pwm.setPWM(R1.ch[1], 0, pulseFemur);
+  pwm.setPWM(R1.ch[2], 0, pulseTibia);
+
+  pwm.setPWM(R2.ch[0], 0, pulseCoxa);
+  pwm.setPWM(R2.ch[1], 0, pulseFemur);
+  pwm.setPWM(R2.ch[2], 0, pulseTibia);
+
+  pwm.setPWM(R3.ch[0], 0, pulseCoxa);
+  pwm.setPWM(R3.ch[1], 0, pulseFemur);
+  pwm.setPWM(R3.ch[2], 0, pulseTibia);
+
+  delay(3000);
+}
+
+void loop() {
+  float y = P3[1];
+
+  for (int i = 0; i <= steps; i++) {
+    float t = (float)i / steps;
+    doMovement(R1, t, y);
+    doMovement(R2, t, y);
+    doMovement(R3, t, y);
+    delay(30);
+  }
+}
