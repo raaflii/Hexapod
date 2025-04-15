c0a79acbef31bd648f8b028f7606736f3df13924
Muhammad Rafli <154863009+raaflii@users.noreply.github.com>
2025-04-15 21:58:20 +0700

    update offset

--- LeftLeg_IK/LeftLeg_IK_Global.ino
+++ LeftLeg_IK/LeftLeg_IK_Global.ino
@@ -9,11 +9,12 @@ struct Leg {
     float offset_x;
     float offset_y;
     float offset_z;
+    float rot_z;
 };
 
-Leg L1 = {{0, 1, 2}, true, -63.0, 83.5, -20.0};
-Leg L2 = {{4, 5, 6}, false, -81.5, 0.0, -20.0};
-Leg L3 = {{8, 9, 10}, true, -63.0, -83.5, -20.0};
+Leg L1 = {{0, 1, 2}, true, -63.0, 83.5, -20.0, 45.0};
+Leg L2 = {{4, 5, 6}, false, -81.5, 0.0, -20.0, 0.0};
+Leg L3 = {{8, 9, 10}, true, -63.0, -83.5, -20.0, -45.0};
 
 const int SERVOMIN = 102; 
 const int SERVOMAX = 512;
@@ -24,6 +25,12 @@ const float tibia_len = 104.0;
 
 float constrainAngle(float val) {
   return constrain(val, 20, 180);
+}
+
+void rotateZ(float x_in, float y_in, float theta_deg, float &x_out, float &y_out) {
+  float theta_rad = radians(theta_deg);
+  x_out = x_in * cos(theta_rad) - y_in * sin(theta_rad);
+  y_out = x_in * sin(theta_rad) + y_in * cos(theta_rad);
 }
 
 void inverseKinematic(Leg &leg, float x, float y, float z, float &sudutCoxa, float &sudutFemur, float &sudutTibia) {
@@ -46,9 +53,14 @@ void inverseKinematic(Leg &leg, float x, float y, float z, float &sudutCoxa, flo
   }
 
 void kakiKiri(Leg &leg, float x, float y, float z, float &servoCoxa, float &servoFemur, float &servoTibia) {
-  float c, f, t;
+  float x_rot, y_rot;
+  rotateZ(x, y, leg.rot_z, x_rot, y_rot);
 
-  inverseKinematic(leg, x, y, z, c, f, t);
+  Serial.print("x_rot: "); Serial.println(x_rot);
+  Serial.print("y_rot: "); Serial.println(y_rot);
+
+  float c, f, t;
+  inverseKinematic(leg, x_rot, y_rot, z, c, f, t);
 
   c = fmod(c, 360);  
   if (c < 0) {
@@ -83,34 +95,29 @@ void loop() {
 
     float servoCoxa, servoFemur, servoTibia;
 
-    Leg legs[3] = {L1, L2, L3};
-    Leg leg;
+    Leg* legPtr = nullptr;
 
     if (Strleg == "L1") {
-        leg = legs[0];
+        legPtr = &L1;
     } else if (Strleg == "L2") {
-        leg = legs[1];
+        legPtr = &L2;
     } else if (Strleg == "L3") {
-        leg = legs[2];
-    } else { 
+        legPtr = &L3;
+    } else {
         Serial.println("Ga valid bg");
         return;
     }
 
-    x -= leg.offset_x;
-    y -= leg.offset_y;
-    z -= leg.offset_z;
+    float x_local = x - legPtr->offset_x;
+    float y_local = y - legPtr->offset_y;
+    float z_local = z - legPtr->offset_z;
 
-    Serial.print("x_local ="); Serial.println(x);
-    Serial.print("x_offset"); Serial.println(leg.offset_x);
+    Serial.println("");
+    Serial.print("x_local ="); Serial.println(x_local);
+    Serial.print("y_local ="); Serial.println(y_local);
+    Serial.print("z_local ="); Serial.println(z_local);
 
-    Serial.print("y_local ="); Serial.println(y);
-    Serial.print("y_offset"); Serial.println(leg.offset_y);
-
-    Serial.print("z_local ="); Serial.println(z);
-    Serial.print("z_offset"); Serial.println(leg.offset_z);
-
-    kakiKiri(leg, x, y, z, servoCoxa, servoFemur, servoTibia);
+    kakiKiri(*legPtr, x_local, y_local, z_local, servoCoxa, servoFemur, servoTibia);
     
     Serial.print("Servo Coxa: ");
     Serial.println(servoCoxa);
@@ -119,13 +126,13 @@ void loop() {
     Serial.print("Servo Tibia: ");
     Serial.println(servoTibia);
     
-    int pulseCoxa  = angleToPulse(servoCoxa);
-    int pulseFemur = angleToPulse(servoFemur);
-    int pulseTibia = angleToPulse(servoTibia);
+    float pulseCoxa  = angleToPulse(servoCoxa);
+    float pulseFemur = angleToPulse(servoFemur);
+    float pulseTibia = angleToPulse(servoTibia);
     
-    pwm.setPWM(leg.ch[0], 0, pulseCoxa);
-    pwm.setPWM(leg.ch[1], 0, pulseFemur);
-    pwm.setPWM(leg.ch[2], 0, pulseTibia);
+    pwm.setPWM(legPtr->ch[0], 0, pulseCoxa);
+    pwm.setPWM(legPtr->ch[1], 0, pulseFemur);
+    pwm.setPWM(legPtr->ch[2], 0, pulseTibia);
   }
   delay(10);
 }
